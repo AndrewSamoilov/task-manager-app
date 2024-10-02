97433377a6a8b473e9abfce136aca8c0c9fb6fba
AndrewSamoilov <andrew.smv94@gmail.com>
2024-10-02 18:37:38 +0300

    add test

--- src/main/java/com/andrewsmv/taskmanagerapp/domain/entity/Category.java
+++ src/main/java/com/andrewsmv/taskmanagerapp/domain/entity/Category.java
@@ -1,14 +1,21 @@
 package com.andrewsmv.taskmanagerapp.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.util.Collections;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Collections;
-import java.util.List;
 
 @Table(name = "category")
 @Entity
@@ -23,8 +30,7 @@ public class Category extends DateStatistic {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
-    @JsonProperty("title")
+    @Column
     private String name;
 
     @OneToMany(mappedBy = "category")
